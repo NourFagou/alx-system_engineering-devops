5eba947a461e3e664ede0a77e0be7909b7968d25
NourFagou <nourfagou@gmail.com>
2024-06-29 11:55:53 -0700

    mandatory

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# install flask
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Ensure the python3-pip package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 and Werkzeug version 2.1.1 using pip3
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  require => Package['python3-pip'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$" && /usr/bin/pip3 show Werkzeug | grep -q "^Version: 2.1.1$"',
 }
