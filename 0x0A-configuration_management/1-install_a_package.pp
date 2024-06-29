5bdf8740400dabd52ad8b26b08f6fd6d703f2242
NourFagou <nourfagou@gmail.com>
2024-06-29 11:25:35 -0700

    mandatory

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,11 @@
-# Ensure the python-pip package is installed
+# Ensure the python3-pip package is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
-# Use the pip provider to install Flask version 2.1.0
-python::pip { 'Flask':
-  ensure       => '2.1.0',
-  pip_provider => 'pip3',
-  require      => Package['python3-pip'],
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  require => Package['python3-pip'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
 }
-
